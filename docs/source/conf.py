3db9d60001c7c3a7b790c19f403973ff0aba7af7
labile <labile.zhu@gmail.com>
2024-04-20 20:56:13 +0800

    bak

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,8 @@ html_theme_options = {
     # "path_to_docs": "docs",
     "use_repository_button": True,
     "use_edit_page_button": False,
-    "show_navbar_depth": 8,
+    "show_navbar_depth": 1,
+    "show_toc_level": 1,
     "logo_only": True,
 }
 
@@ -66,5 +67,3 @@ epub_show_urls = 'footnote'
 
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
-
-latex_documents = []
