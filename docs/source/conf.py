39970a3deb96a1461bfb50b0df32455a0db95d97
labile <labile.zhu@gmail.com>
2024-05-23 22:44:45 +0800

    toctree

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,9 +64,9 @@ language = 'en_US'
 
 # -- Options for EPUB output
 epub_show_urls = 'no'
-epub_tocscope = 'default'
+epub_tocscope = 'includehidden'
 epub_cover = ('_static/book-cover-800.png', '')
-epub_tocdup = True
+epub_tocdup = False
 
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
